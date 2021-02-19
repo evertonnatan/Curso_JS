dc33394ef5cd1831b3bf0aa3e5642b86b5a2a4a3
evertonnatan <evertonnatandl@outlook.com>
2021-02-19 08:50:52 -0300

    Iniciando o curso de JS do basico ao avançado

---
+++ 01-tipos-de-dados/numbers.js
@@ -0,0 +1,7 @@
+console.log(typeof 5)
+console.log(typeof 12.7)
+console.log(typeof -128)
+
+// Revisão e aprofundamento:
+
+// https://docs.google.com/document/d/1gfFejHvZgS_5GaHcBMaFF1yepwe3cjZIh9kZ173xEGk/edit?usp=sharing
---
+++ 01-tipos-de-dados/operacoes_aritmeticas.js
@@ -0,0 +1,12 @@
+console.log(2 + 3)
+console.log(5 - 12)
+console.log(4 * 4)
+console.log(5 / 3)
+console.log(20 % 2)
+console.log(21 % 2)
+
+// O resultado de operações aritméticas envolvendo numbers, são dados, também, do tipo number.
+
+// Revisão e aprofundamento:
+
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators

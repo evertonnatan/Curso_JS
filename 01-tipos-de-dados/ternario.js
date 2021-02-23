849d190aa723e1192c569339368a9f6fc05a12b0
evertonnatan <evertonnatandl@outlook.com>
2021-02-23 06:45:09 -0300

    Aula de ternario

---
+++ 01-tipos-de-dados/ternario.js
@@ -0,0 +1,5 @@
+// Se 5 for maior do que 2, retornará a primeira sentença (a que vem antes dos dois pontos). Se não, retornará a segunda sentença.
+
+console.log(5 > 2 ? 'Sim, ele é' : 'Não, ele não é')
+console.log('Natã' == 'Natã' ? 'Sim, ele é um excelente programador' : 'Não, ele ainda tem muito o que aprender')
+
